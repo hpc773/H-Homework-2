46ebb3606a29eaeeaa8dead6dd5aefd3187f3d39
kcgbl <kcgbl@the-machine>
2024-02-09 16:57:03 -0600

    Created displayStudents method

--- src/homework_2/StudentManager.java
+++ src/homework_2/StudentManager.java
@@ -1,6 +1,25 @@
 package homework_2;
 
 public class StudentManager {
+	
 	private Student[] students;
+	
+	public void displayStudents() { 
+		int check = 0; 
+		//Loop iterates through students toStringing each one 
+		for (Student student : students) {
+			if (student != null){
+				
+				toString();
+				check++;
+			}
+			// if the loop runs once and finds check to equal 0 then there are no students in the array.
+			else if (check ==0){
+				System.out.println(" There are no students in the Student array");
+			}
+		}
+		
+		
+	}
 
 }
