693abd8c5e3fb5c47dcd804dac0290e0b22ebb52
Schwick3 <sky321949@gmail.com>
2024-02-09 18:00:31 -0600

    homework 2 file

--- src/homework_2/Homework_2.java
+++ src/homework_2/Homework_2.java
@@ -5,7 +5,14 @@ public class Homework_2 {
 	public static void main(String[] args) {
 		System.out.println("hello test");
 		
-
+		Student student1 = new Student(1, "Skylar", 4);
+		
+		StudentManager teacher = new StudentManager();
+		
+		teacher.addStudent(student1);
+		teacher.searchStudentByID(1);
+		
+	
 	}
 
 }
